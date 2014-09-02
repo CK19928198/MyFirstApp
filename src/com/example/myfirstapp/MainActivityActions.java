fc6e495558b923179fa5815b7b3f0b867c4f3352
CK19928198 <ck19928198@gmail.com>
2014-09-02 10:56:17 +0800

    Initial commit

---
+++ src/com/example/myfirstapp/MainActivityActions.java
@@ -0,0 +1,53 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivityActions extends ActionBarActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_display_message);
+
+	    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	    // If your minSdkVersion is 11 or higher, instead use:
+	    // getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            openSearch();
+	            return true;
+	        case R.id.action_settings:
+	            openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+
+	private void openSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void openSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+}
